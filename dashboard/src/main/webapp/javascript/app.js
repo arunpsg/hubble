378b0ab20fdbaa05273a5fe9a12443bb3cc268a3
Arun <arun.kumar-r@hp.com>
2015-03-10 18:42:04 -0700

    adding angularjs support

---
+++ dashboard/src/main/webapp/javascript/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('formApp', []);
+
+app.config( function( $controllerProvider ) {
+    $controllerProvider.register( 'dashController', function( $scope) {
+    }
+}
+
+app.controller('dashController', [ '$scope', function($scope) {
+
+	
+} ]);
