57f26958205ec9e7dc02c90c9e90dc49efed28c3
Arun <arun.kumar-r@hp.com>
2015-03-12 02:22:50 -0700

    Title & Chevron changes

--- dashboard/src/main/webapp/javascript/app.js
+++ dashboard/src/main/webapp/javascript/app.js
@@ -81,7 +81,16 @@ app.directive("markable", function() {
             		elem.addClass("glyphicon-check");            		
                     elem.parent().find(".taskName").addClass("toggleColor");
             	}
-                
+
+            	if(elem.hasClass('glyphicon-check')){
+            		elem.removeClass("glyphicon-check");
+            		elem.addClass("glyphicon-unchecked");
+            	}
+            	else if (elem.hasClass("glyphicon-unchecked")){
+            		elem.removeClass("glyphicon-unchecked");
+            		elem.addClass("glyphicon-check");
+               
+            	}
             });
         }
     };
@@ -145,49 +154,6 @@ app.directive("checkCollapseOne", function(){
 		templateUrl: '/dashboard/check_collapseOne.html'
 	};
 });
-
-
-
-angular.module('docsTabsExample', [])
-.directive('myTabs', function() {
-  return {
-    restrict: 'E',
-    transclude: true,
-    scope: {},
-    controller: function($scope) {
-      var panes = $scope.panes = [];
-
-      $scope.select = function(pane) {
-        angular.forEach(panes, function(pane) {
-          pane.selected = false;
-        });
-        pane.selected = true;
-      };
-
-      this.addPane = function(pane) {
-        if (panes.length === 0) {
-          $scope.select(pane);
-        }
-        panes.push(pane);
-      };
-    },
-    templateUrl: 'my-tabs.html'
-  };
-})
-.directive('myPane', function() {
-  return {
-    require: '^myTabs',
-    restrict: 'E',
-    transclude: true,
-    scope: {
-      title: '@'
-    },
-    link: function(scope, element, attrs, tabsCtrl) {
-      tabsCtrl.addPane(scope);
-    },
-    templateUrl: 'my-pane.html'
-  };
-});});
 
 
 
